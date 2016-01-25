e1f74ca52a6bcdae7d58483c8bf5603a48a0a770
xuyanfei <xuyanfei@goodix.com>
2016-01-26 00:11:13 +0800

    l:show STASH over

--- hello.c
+++ hello.c
@@ -1,3 +1,4 @@
 #include<stdio.h>
 lA:add 2329 change to 2343
 lA:add 2344
+lA:div 2329
--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
 void main(){
 	printf("--lA:add 2330 --change to 2341");	
+	printf("lA:add 2330");	
+	printf("l:add 2352");
 }
