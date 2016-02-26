f2a834703d8f25bd1ee0029bd4ef7e0f49325a75
xuyanfei <xuyanfei@goodix.com>
2016-02-26 16:48:56 +0800

    delete nouse files

--- hello.c
+++
@@ -1,4 +0,0 @@
-#include<stdio.h>
-lA:add 2329 change to 2343
-lA:add 2344
-lA:div 2329
--- hello.java
+++
@@ -1,5 +0,0 @@
-void main(){
-	printf("--lA:add 2330 --change to 2341");	
-	printf("lA:add 2330");	
-	printf("l:add 2352");
-}
